af2984e1f537664dfa8df8a6a6944a72962582b9
Mehwish Aziz <mehwisha@msn.com>
2018-08-14 03:41:35 +0000

    automatically backed up by learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,12 +1,4 @@
 # Create your CostumeStore class here
 class CostumeStore < ActiveRecord::Migration[5.1]
-def change
-create_table : CostumeStore do |t|
-  t.string :name
-  t.string :location
-  t.integer :costume_invenntory
-  t.string :active
-  t.timestamp :opening
-  t.timestamp :closing
-end
+
 end
